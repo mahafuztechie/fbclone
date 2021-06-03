cae94d0cf0ed5c1add0e9bb53749e3848556fcdd
mahafuztechie <mahafuzaadeez5@gmail.com>
2021-06-03 14:41:51 +0530

    completed

--- components/Feed.js
+++ components/Feed.js
@@ -1,21 +1,18 @@
-import React from 'react'
-import InputBox from './InputBox'
-import Posts from './Posts'
-import Stories from './Stories'
+import React from "react";
+import InputBox from "./InputBox";
+import Stories from "./Stories";
+import Posts from "./Posts";
 
-function Feed() {
-    return (
-        <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto">
-            <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
-                {/* {stories} */}
-                <Stories />
-                {/* {Input Box} */}
-                <InputBox />
-                {/* {Posts} */}
-                <Posts />
-            </div>
-        </div>
-    )
+function Feed({ posts }) {
+  return (
+    <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto scrollbar-hide">
+      <div className="mx-auto max-w-md md:max-w-lg lg:max-w-2xl">
+        <Stories />
+        <InputBox />
+        <Posts posts={posts} />
+      </div>
+    </div>
+  );
 }
 
-export default Feed
+export default Feed;
